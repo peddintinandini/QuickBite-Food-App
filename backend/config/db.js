fe7256ed8c8bc1708bb8f9346f9d80acb22558d1
Nandini P <96322585+peddintinandini@users.noreply.github.com>
2026-05-23 09:48:09 +0530

    Update MongoDB connection string to remove credentials
    Removed sensitive credentials from MongoDB connection string.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://peddintinandini1:Nandu2973@fooddel.z7zf9lo.mongodb.net/fooddel=fooddel').then(()=>console.log("DB connected"));  
+    await mongoose.connect('mongodb+srv://').then(()=>console.log("DB connected"));  
 }
